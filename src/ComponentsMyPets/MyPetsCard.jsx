8e2d33249f4700d6a1036fc016c3694879a6f780
Mateus Rosengarten Mauricio <108750288+Mateus1rosengarten@users.noreply.github.com>
2023-11-27 01:48:17 -0300

    first commit

---
+++ src/ComponentsMyPets/MyPetsCard.jsx
@@ -0,0 +1,45 @@
+
+import { Box,Card, CardHeader, CardBody, CardFooter, Divider, Button, ChakraProvider,CSSReset, } from '@chakra-ui/react'
+
+import { MDBCardImage } from 'mdb-react-ui-kit';
+import { Stack,Text,Image } from "@chakra-ui/react";
+import { Heading } from "@chakra-ui/react";
+import { ButtonGroup } from "react-bootstrap";
+
+function MyPetsCard ({cardImage,cardName,cardStatus,handleButton}) {
+  const buttonSize = window.innerWidth >= 768 ? 'md' : 'md'; // Adjust the breakpoint as needed
+  const marginSize = window.innerWidth >= 1200? '0.vw' : '0';
+  const nameSize = window.innerWidth >= 768 ? 'md' : 'sm';
+  const widthSize = window.innerWidth >= 1200 ? '7vw' : '23vw'
+
+    return (
+        <ChakraProvider>
+            <CSSReset/> 
+        <Card className='shake' 
+       transition="transform 0.1s ease-in-out" _hover={{
+        transform: "rotate(10deg)", 
+      }}
+         >
+  <CardBody>
+  <Image borderRadius='lg' src={cardImage} />
+    <Stack mt='6' spacing='3'>
+      <Heading  size={nameSize} marginLeft='1.5vw'>{cardName.toUpperCase()}</Heading>
+      
+ </Stack>
+  </CardBody>
+  <Divider />
+  <CardFooter>
+    <ButtonGroup>
+    <Box  display="flex"justifyContent="center" alignItems="center">
+      <Button onClick={handleButton} size={buttonSize} variant='solid' colorScheme='blue' marginLeft={marginSize} marginBottom='1vh' width={widthSize} >
+        See more
+      </Button >
+      </Box>
+    </ButtonGroup>
+  </CardFooter>
+</Card> 
+
+</ChakraProvider> )
+}
+
+export default MyPetsCard;
---
+++ src/ComponentsMyPets/PetCard.jsx
@@ -0,0 +1,27 @@
+import "./PetCard.css";
+
+function PetCard({ imag,name, status, type, hei, wei, color, bio, breed,}) {
+  return (
+    <div className="card">
+      <img src="" alt="" />
+     
+        <img className="img-card" src={imag} alt=""></img>
+        <div className="info-card">
+        <h6 className="h6-card">Name : </h6> <h6 className="h6-result"> {name}</h6> 
+        <h6 className="h6-card">Status : </h6> <h6 className="h6-result">  {status}</h6>
+        <h6 className="h6-card">Type : </h6> <h6 className="h6-result">  {type}</h6> 
+        <h6 className="h6-card">Heigth :  </h6>  <h6 className="h6-result">  {hei} cm</h6>
+        <h6 className="h6-card">Weigth : </h6> <h6 className="h6-result">  {wei} kg</h6>
+        <h6 className="h6-card">Color :</h6><h6 className="h6-result">  {color}</h6>
+        <h6 className="h6-card">Bio :  </h6><h6 className="h6-result">  {bio} </h6>
+        <h6 className="h6-card">Breed :  </h6><h6 className="h6-result">  {breed}</h6>
+
+        
+        
+        
+      </div>
+    </div>
+  );
+}
+
+export default PetCard;
