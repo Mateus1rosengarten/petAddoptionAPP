7b4fe8f4712242e77f2bb954006c42ab4109c5bb
Mateus Rosengarten Mauricio <108750288+Mateus1rosengarten@users.noreply.github.com>
2024-01-22 18:58:37 -0300

    commit new card design

--- src/ComponentsMyPets/MyPetsCard.jsx
+++ src/ComponentsMyPets/MyPetsCard.jsx
@@ -1,13 +1,11 @@
 
-import { Box,Card, CardHeader, CardBody, CardFooter, Divider, Button, ChakraProvider,CSSReset, } from '@chakra-ui/react'
-
-import { MDBCardImage } from 'mdb-react-ui-kit';
-import { Stack,Text,Image } from "@chakra-ui/react";
+import { Box,Card, CardBody, CardFooter, Divider, Button, ChakraProvider,CSSReset, } from '@chakra-ui/react'
+import { Stack,Image } from "@chakra-ui/react";
 import { Heading } from "@chakra-ui/react";
 import { ButtonGroup } from "react-bootstrap";
 
 function MyPetsCard ({cardImage,cardName,cardStatus,handleButton}) {
-  const buttonSize = window.innerWidth >= 768 ? 'md' : 'md'; // Adjust the breakpoint as needed
+  const buttonSize = window.innerWidth >= 768 ? 'md' : 'md'; 
   const marginSize = window.innerWidth >= 1200? '0.vw' : '0';
   const nameSize = window.innerWidth >= 768 ? 'md' : 'sm';
   const widthSize = window.innerWidth >= 1200 ? '7vw' : '23vw'
@@ -16,22 +14,23 @@ function MyPetsCard ({cardImage,cardName,cardStatus,handleButton}) {
         <ChakraProvider>
             <CSSReset/> 
         <Card className='shake' 
+       
        transition="transform 0.1s ease-in-out" _hover={{
         transform: "rotate(10deg)", 
       }}
          >
   <CardBody>
-  <Image borderRadius='lg' src={cardImage} />
-    <Stack mt='6' spacing='3'>
-      <Heading  size={nameSize} marginLeft='1.5vw'>{cardName.toUpperCase()}</Heading>
+  <Image borderRadius='lg' src={cardImage} height='12vh' width='100%'/>
+    <Stack mt='6'>
+      <Heading textAlign='center'  size={nameSize}>{cardName.toUpperCase()}</Heading>
       
  </Stack>
   </CardBody>
   <Divider />
-  <CardFooter>
+  <CardFooter display="flex" justifyContent="center" alignItems="center">
     <ButtonGroup>
-    <Box  display="flex"justifyContent="center" alignItems="center">
-      <Button onClick={handleButton} size={buttonSize} variant='solid' colorScheme='blue' marginLeft={marginSize} marginBottom='1vh' width={widthSize} >
+    <Box>
+      <Button onClick={handleButton} size={buttonSize} variant='solid' colorScheme='blue'  width={widthSize} >
         See more
       </Button >
       </Box>
